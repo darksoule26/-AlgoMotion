f795fd8d1c47ecd870ecec93870dae33932684c4
dead-shadow-7 <deadshadow12119@gmail.com>
2025-03-16 23:11:19 +0530

    Added debugCode Controller

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { analyzeCode } from "../controller/analyzeCodeController.js";
 import { root } from "../controller/rootController.js";
+import { analyzeCode } from "../controller/analyzeCodeController.js";
+import { debugCode } from "../controller/debugCodeController.js";
 const router = express.Router();
 
 router.get("/", root);
 router.post("/analyze", analyzeCode);
+router.post("/debug", debugCode);
 
 export default router;
